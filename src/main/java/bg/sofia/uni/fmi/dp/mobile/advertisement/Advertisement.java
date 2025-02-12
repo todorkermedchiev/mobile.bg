a35e22bd0602e14a9a810f2a4b4edd2e35237efb
todorkermedchiev <todorkerm@gmail.com>
2025-02-12 21:53:38 +0200

    Add price stats to AdvertisementRepository

--- src/main/java/bg/sofia/uni/fmi/dp/mobile/advertisement/Advertisement.java
+++ src/main/java/bg/sofia/uni/fmi/dp/mobile/advertisement/Advertisement.java
@@ -2,17 +2,21 @@ package bg.sofia.uni.fmi.dp.mobile.advertisement;
 
 import bg.sofia.uni.fmi.dp.mobile.vehicle.Vehicle;
 
+import java.time.LocalDateTime;
+
 public record Advertisement(
         String title,
         double price,
         Vehicle vehicle,
         String description,
-        String location
+        String location,
+        LocalDateTime createdAt
 ) {
     @Override
     public String toString() {
         return "Advertisement{" +
-                "title='" + title + '\'' +
+                "createdAt=" + createdAt +
+                ", title='" + title + '\'' +
                 ", price=" + price +
                 ", vehicle=" + vehicle +
                 ", description='" + description + '\'' +
